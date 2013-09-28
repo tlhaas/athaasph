ca79ae47b7e1d56b9070b46373690b9f44517f2f
Timothy Haas <tihaas@Timothys-MacBook-Pro.local>
2013-09-28 12:33:48 -0400

    initial commit

---
+++ MySQLDatabase.rb
@@ -0,0 +1,114 @@
+require 'mysql2'
+require 'digest'
+
+class MySQLDatabase
+
+	attr_accessor :host, :port, :username, :password, :database, :connection
+
+	def initialize(host='127.0.0.1', port=8889, username='root', password='root', database='athaasph')
+		self.host 		= host
+		self.port 		= port
+		self.username 	= username
+		self.password 	= password
+		self.database	= database
+	end # end initialize
+
+	def connect
+		self.connection = Mysql2::Client.new(:host => self.host, :port => self.port, :username => self.username, :password => self.password, :database => self.database);
+	end # end connect
+
+	def close 
+		self.connection.close
+	end
+
+	# Select
+	def get(sql)
+		begin
+			results = self.connection.query(sql)
+			resp	= Array.new
+			results.each do |row|
+			  resp.push(row)
+			end
+		rescue Mysql2::Error => e
+			raise e.message
+			# log errors in the future
+		end
+
+		return resp
+	end # end get
+
+	# Insert
+	def post(sql)
+		begin
+			results = self.connection.query(sql)
+			last_id = self.connection.last_id
+		rescue Mysql2::Error => e
+			raise e.message
+			nil
+		end
+	end
+
+	# Update
+	def put(sql)
+		begin 
+			results 	= self.connection.query(sql)
+			num_rows 	= self.connection.affected_rows
+		rescue Mysql2::Error => e
+			raise e.message
+		end
+
+		if num_rows >= 1
+			return true
+		else
+			return false
+		end
+	end
+
+	# Delete 
+	def delete(sql)
+		begin 
+			results 	= self.connection.query(sql)
+			num_rows 	= self.connection.affected_rows
+		rescue MySQL2::Error => e
+			raise e.message
+		end
+
+		if num_rows >= 1
+			return true
+		else
+			return false
+		end
+	end
+
+end # end class
+
+#
+#------------------------------------------------------------------#
+#
+
+# Let's try it out now
+=begin
+db 		= MySQLDatabase.new()
+db.connect
+	
+	resp 	= db.get("SELECT max(id) as max_id FROM user")
+	next_id = resp[0]["max_id"] + 1
+
+	username		= "Fat Tits 4.0"
+	hashed_password = Digest::SHA1.hexdigest("sandynips")
+	auth 			= "admin"
+
+	resp 	= db.post("INSERT INTO user (id, username, password, auth) VALUES (\"#{next_id}\", \"#{username}\", \"#{hashed_password}\", \"#{auth}\")")
+	puts "Insert ID: " + resp.inspect
+
+	new_username 	= "Fat Tits 12.0"
+	user_id 		= next_id
+	resp 	= db.put("UPDATE user SET username='#{new_username}' WHERE id='#{user_id}'")
+	puts "Username updated? " + resp.inspect
+
+	id = next_id
+	resp 	= db.delete("DELETE FROM user WHERE id='#{id}'")
+	puts "User deleted? " + resp.inspect
+
+db.close
+=end
---
+++ User.rb
@@ -0,0 +1,192 @@
+require "digest"
+require "./MySQLDatabase"
+
+class User
+
+	attr_accessor :id, :customer_id, :username, :auth, :password
+
+	def initialize(options={})
+		self.id 			= options[:id]
+		self.customer_id 	= options[:customer_id]
+		self.username 		= options[:username]
+		self.auth 			= options[:auth]
+		self.password 		= options[:password]
+	end
+
+	# formerly getUser
+	def fetch()
+		begin
+			sql = "SELECT id, customer_id, username, auth, password FROM user WHERE id = #{self.id} LIMIT 1"
+			db = MySQLDatabase.new()
+			db.connect
+			resp = db.get(sql)
+			db.close
+
+			resp.each do |row|
+				self.id = resp[0]['id']
+				self.customer_id = resp[0]['customer_id']
+				self.username = resp[0]['username']
+				self.auth = resp[0]['auth']
+				self.password = resp[0]['password']
+			end
+		rescue Exception => e
+			raise e.message
+		ensure
+			db.close
+		end
+
+	end
+
+	# formerly createUser
+	def post()
+		begin
+			hashed_password = Digest::SHA1.hexdigest(self.password)
+			sql = "SELECT max(id) as max_id FROM user"
+			db 	= MySQLDatabase.new()
+			db.connect
+			resp 	= db.get(sql)
+			next_id = resp[0]["max_id"] + 1
+
+			if self.customer_id
+				sql = "INSERT INTO user (id, customer_id, username, password, auth) VALUES (\"#{next_id}\", \"#{self.customer_id}\", \"#{self.username}\", \"#{hashed_password}\", \"#{self.auth}\")"
+			else
+				sql = "INSERT INTO user (id, username, password, auth) VALUES (\"#{next_id}\", \"#{self.username}\", \"#{self.password}\", \"#{self.auth}\")"
+			end
+			resp 	= db.post(sql)
+		rescue Exception => e
+			raise e.message
+		ensure
+			db.close		
+		end
+		
+		self.id = resp unless resp.nil?
+	end
+
+	def update_password()
+		hashed_password = Digest::SHA1.hexdigest(self.password)
+		begin
+			sql = "UPDATE user SET password='#{hashed_password}' WHERE id='#{self.id}'"
+			db = MySQLDatabase.new()
+			db.connect
+			resp = db.put(sql)
+		rescue Exception => e
+			raise e.message
+		ensure
+			db.close
+		end
+	end
+
+	def update_username()
+		begin 
+			sql = "UPDATE user SET username='#{self.username}' WHERE id='#{self.id}'"
+			db = MySQLDatabase.new()
+			db.connect
+			resp = db.put(sql)
+		rescue Exception => e
+			raise e.message
+		ensure
+			db.close
+		end
+	end
+
+	def update_auth()
+		begin
+			sql = "UPDATE user SET auth='#{self.auth}' WHERE id='#{self.id}'"
+			db = MySQLDatabase.new()
+			db.connect
+			resp = db.put(sql)
+		rescue Exception => e
+			raise e.message
+		ensure
+			db.close
+		end
+	end
+
+	def delete()
+		begin
+			sql = "DELETE FROM user WHERE id='#{id}'"
+			db = MySQLDatabase.new()
+			db.connect
+			resp = db.delete(sql)
+		rescue Exception => e
+			raise e.message
+		ensure
+			db.close
+		end
+	end
+
+end
+
+#------------------------------------------------#
+
+#
+# delete
+# 
+
+=begin
+begin
+	user = User.new(:id => "7")
+	puts user.delete
+rescue Exception => e
+	puts e.message
+end
+=end
+
+#------------------------------------------------#
+
+#
+# put
+# 
+
+=begin
+begin
+	user = User.new(:id => "1")
+	user.fetch()
+	puts user.inspect
+	puts "Auth before: #{user.auth}"
+	user.auth = "admin"
+	user.update_auth()
+	user.fetch()
+	puts "Auth after: #{user.auth}"
+rescue Exception => e
+	puts e.message
+end
+=end
+
+#------------------------------------------------#
+
+#
+# get
+# 
+
+=begin
+begin
+	user = User.new(:id => "1")
+	user.fetch()
+	puts user.inspect
+rescue Exception => e
+	puts "Banana boobs"
+end 
+=end 
+
+#------------------------------------------------#
+
+#
+# post
+# 
+=begin
+user_deets = {
+	:username 	=> "Larry Pants Dude 5.0",
+	:password 	=> "Penistits",
+	:auth 		=> "admin"
+}
+
+begin 
+	user = User.new(user_deets)
+	user.post()
+rescue Exception => e
+	puts "404, bro"
+end
+
+puts user.inspect
+=end
