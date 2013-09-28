ff4b285f4025ebd024ac66fa37155b3d6e0a4459
Timothy Haas <tihaas@Timothys-MacBook-Pro.local>
2013-09-28 19:08:05 -0400

    Cleanup spacing in User.rb

--- User.rb
+++ User.rb
@@ -6,113 +6,112 @@ class User
 	attr_accessor :id, :customer_id, :username, :auth, :password
 
 	def initialize(options={})
-		self.id 			= options[:id]
-		self.customer_id 	= options[:customer_id]
-		self.username 		= options[:username]
-		self.auth 			= options[:auth]
-		self.password 		= options[:password]
+	  self.id 			= options[:id]
+	  self.customer_id 	= options[:customer_id]
+	  self.username 	= options[:username]
+	  self.auth 		= options[:auth]
+	  self.password 	= options[:password]
 	end
 
 	# formerly getUser
 	def fetch()
-		begin
-			sql = "SELECT id, customer_id, username, auth, password FROM user WHERE id = #{self.id} LIMIT 1"
-			db = MySQLDatabase.new()
-			db.connect
-			resp = db.get(sql)
-			db.close
+	  begin
+	    sql = "SELECT id, customer_id, username, auth, password FROM user WHERE id = #{self.id} LIMIT 1"
+		db = MySQLDatabase.new()
+		db.connect
+		resp = db.get(sql)
+		db.close
 
-			resp.each do |row|
-				self.id = resp[0]['id']
-				self.customer_id = resp[0]['customer_id']
-				self.username = resp[0]['username']
-				self.auth = resp[0]['auth']
-				self.password = resp[0]['password']
-			end
-		rescue Exception => e
-			raise e.message
-		ensure
-			db.close
-		end
-
+		resp.each do |row|
+		  self.id = resp[0]['id']
+		  self.customer_id = resp[0]['customer_id']
+		  self.username = resp[0]['username']
+		  self.auth = resp[0]['auth']
+		  self.password = resp[0]['password']
+	  end
+	  rescue Exception => e
+		raise e.message
+	  ensure
+		db.close
+	  end
 	end
 
 	# formerly createUser
 	def post()
-		begin
-			hashed_password = Digest::SHA1.hexdigest(self.password)
-			sql = "SELECT max(id) as max_id FROM user"
-			db 	= MySQLDatabase.new()
-			db.connect
-			resp 	= db.get(sql)
-			next_id = resp[0]["max_id"] + 1
+	  begin
+	    hashed_password = Digest::SHA1.hexdigest(self.password)
+		sql = "SELECT max(id) as max_id FROM user"
+		db 	= MySQLDatabase.new()
+		db.connect
+		resp 	= db.get(sql)
+		next_id = resp[0]["max_id"] + 1
 
-			if self.customer_id
-				sql = "INSERT INTO user (id, customer_id, username, password, auth) VALUES (\"#{next_id}\", \"#{self.customer_id}\", \"#{self.username}\", \"#{hashed_password}\", \"#{self.auth}\")"
-			else
-				sql = "INSERT INTO user (id, username, password, auth) VALUES (\"#{next_id}\", \"#{self.username}\", \"#{self.password}\", \"#{self.auth}\")"
-			end
-			resp 	= db.post(sql)
-		rescue Exception => e
-			raise e.message
-		ensure
-			db.close		
+		if self.customer_id
+		  sql = "INSERT INTO user (id, customer_id, username, password, auth) VALUES (\"#{next_id}\", \"#{self.customer_id}\", \"#{self.username}\", \"#{hashed_password}\", \"#{self.auth}\")"
+		else
+		  sql = "INSERT INTO user (id, username, password, auth) VALUES (\"#{next_id}\", \"#{self.username}\", \"#{self.password}\", \"#{self.auth}\")"
 		end
+		resp 	= db.post(sql)
+	  rescue Exception => e
+		raise e.message
+	  ensure
+		db.close		
+	  end
 		
-		self.id = resp unless resp.nil?
+	  self.id = resp unless resp.nil?
 	end
 
 	def update_password()
-		hashed_password = Digest::SHA1.hexdigest(self.password)
-		begin
-			sql = "UPDATE user SET password='#{hashed_password}' WHERE id='#{self.id}'"
-			db = MySQLDatabase.new()
-			db.connect
-			resp = db.put(sql)
-		rescue Exception => e
-			raise e.message
-		ensure
-			db.close
-		end
+	  hashed_password = Digest::SHA1.hexdigest(self.password)
+	  begin
+		sql = "UPDATE user SET password='#{hashed_password}' WHERE id='#{self.id}'"
+		db = MySQLDatabase.new()
+		db.connect
+		resp = db.put(sql)
+	  rescue Exception => e
+		raise e.message
+	  ensure
+		db.close
+	  end
 	end
 
 	def update_username()
-		begin 
-			sql = "UPDATE user SET username='#{self.username}' WHERE id='#{self.id}'"
-			db = MySQLDatabase.new()
-			db.connect
-			resp = db.put(sql)
-		rescue Exception => e
-			raise e.message
-		ensure
-			db.close
-		end
+	  begin 
+		sql = "UPDATE user SET username='#{self.username}' WHERE id='#{self.id}'"
+		db = MySQLDatabase.new()
+		db.connect
+		resp = db.put(sql)
+	  rescue Exception => e
+		raise e.message
+	  ensure
+		db.close
+	  end
 	end
 
 	def update_auth()
-		begin
-			sql = "UPDATE user SET auth='#{self.auth}' WHERE id='#{self.id}'"
-			db = MySQLDatabase.new()
-			db.connect
-			resp = db.put(sql)
-		rescue Exception => e
-			raise e.message
-		ensure
-			db.close
-		end
+	  begin
+		sql = "UPDATE user SET auth='#{self.auth}' WHERE id='#{self.id}'"
+		db = MySQLDatabase.new()
+		db.connect
+		resp = db.put(sql)
+	  rescue Exception => e
+		raise e.message
+	  ensure
+		db.close
+	  end
 	end
 
 	def delete()
-		begin
-			sql = "DELETE FROM user WHERE id='#{id}'"
-			db = MySQLDatabase.new()
-			db.connect
-			resp = db.delete(sql)
-		rescue Exception => e
-			raise e.message
-		ensure
-			db.close
-		end
+	  begin
+		sql = "DELETE FROM user WHERE id='#{id}'"
+		db = MySQLDatabase.new()
+		db.connect
+		resp = db.delete(sql)
+	  rescue Exception => e
+		raise e.message
+	  ensure
+		db.close
+	  end
 	end
 
 end
